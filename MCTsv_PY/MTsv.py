0f24c89404862abcc105498a35a87f5d0271fbef
Minamin <62072288+Minamin1234@users.noreply.github.com>
2022-01-20 16:54:56 +0900

    MTsvクラスの関数を宣言。

--- MCTsv_PY/MTsv.py
+++ MCTsv_PY/MTsv.py
@@ -1,3 +1,20 @@
 class MTsv(object):
+    ENCODING:str = "UTF-8"
+
     def __init__(self):
+        return
+
+    def ToTSVFromList(self,words:List[List[str]]) -> str:
+        return
+
+    def ToListFromTSV(self,words:str) -> List[List[str]]:
+        return
+
+    def RemakeTSV(self,words:str) -> str:
+        return
+
+    def GetTextFromTSVFile(self,path:str) -> str:
+        return
+
+    def ToCSV(self,TSVtext:str) -> str:
         return
