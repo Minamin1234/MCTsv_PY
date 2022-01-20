18505086171cbfa6cafba1b42e9e6561682d24f1
Minamin <62072288+Minamin1234@users.noreply.github.com>
2022-01-20 16:33:39 +0900

    MCsvの関数を宣言。

--- MCTsv_PY/MCsv.py
+++ MCTsv_PY/MCsv.py
@@ -2,8 +2,21 @@ from typing import NoReturn as void
 from typing import List
 
 class MCsv(object):
-    def __init__(self) -> None:
+    ENCODING:str = "UTF-8"
+
+    def __init__(self) -> void:
         return
+
+    def ToCSVFromList(self,words:List[str]) -> str:
+        return
+
+    def ToListFromCSV(self,words:str) -> List[str]:
+        return
+
+    def RemakeCSV(self,words:str): -> str:
+        return
+
+
 
 
 
